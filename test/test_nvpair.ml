00e1abfc2924a4f880dc98655639e5f430010121
Ryan Moeller <ryan-moeller@att.net>
2024-04-12 19:57:20 -0400

    Expand test

--- test/test_nvpair.ml
+++ test/test_nvpair.ml
@@ -12,7 +12,7 @@ let () =
     match Nvlist.next_nvpair pools pair with
     | None -> ()
     | Some p ->
-      print_endline @@ Nvpair.name p;
+      Printf.printf "\t%s\n" @@ Nvpair.name p;
       loop @@ Some p
   in
   print_endline "Pools in cache file:";
@@ -26,4 +26,29 @@ let () =
   let raidz = Array.get top_vdevs 0 in
   let raidz_children = Option.get @@ Nvlist.lookup_nvlist_array raidz "children" in
   print_endline "storage pool vdevs:";
-  Array.iter (fun child -> print_endline @@ Option.get @@ Nvlist.lookup_string child "path") raidz_children
+  Array.iter (fun child -> Printf.printf "\t%s\n" @@ Option.get @@ Nvlist.lookup_string child "path") raidz_children
+
+let () =
+  let version = Option.get @@ Nvlist.lookup_uint64 storage "version" in
+  let txg = Option.get @@ Nvlist.lookup_uint64 storage "txg" in
+  let pool_guid = Option.get @@ Nvlist.lookup_uint64 storage "pool_guid" in
+  let hostid = Option.get @@ Nvlist.lookup_uint64 storage "hostid" in
+  let hostname = Option.get @@ Nvlist.lookup_string storage "hostname" in
+  print_endline "storage pool details:";
+  Printf.printf "\tversion: %Lu\n" version;
+  Printf.printf "\ttxg: %Lu\n" txg;
+  Printf.printf "\tpool_guid: %Lx\n" pool_guid;
+  Printf.printf "\thostid: %Lu\n" hostid;
+  Printf.printf "\thostname: %s\n" hostname
+
+let () =
+  let features_for_read = Option.get @@ Nvlist.lookup_nvlist storage "features_for_read" in
+  let rec loop pair =
+    match Nvlist.next_nvpair features_for_read pair with
+    | None -> ()
+    | Some p ->
+      Printf.printf "\t%s\n" @@ Nvpair.name p;
+      loop @@ Some p
+  in
+  print_endline "storage pool features required for read:";
+  loop None
